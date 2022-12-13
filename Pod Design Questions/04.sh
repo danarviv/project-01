25679a59c299be9b9fab0a0d34140ab0ddb0dbc9
Dan Arviv <120067574+danarviv@users.noreply.github.com>
2022-12-13 02:16:20 +0200

    Create 04.sh

---
+++ Pod Design Questions/04.sh
@@ -0,0 +1,6 @@
+kubectl get pod -l env=dev
+
+NAME      READY   STATUS    RESTARTS   AGE
+nginx-3   1/1     Running   0          4m
+nginx-4   1/1     Running   0          4m
+nginx-5   1/1     Running   0          4m
