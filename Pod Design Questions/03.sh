92fbaeadda574b7efec89e01859d9a7df3728b32
Dan Arviv <120067574+danarviv@users.noreply.github.com>
2022-12-13 02:12:30 +0200

    Create 03.sh

---
+++ Pod Design Questions/03.sh
@@ -0,0 +1,8 @@
+ kubectl get pod --show-labels
+ 
+ NAME      READY   STATUS    RESTARTS   AGE   LABELS
+nginx-1   1/1     Running   0          19s   env=prod
+nginx-2   1/1     Running   0          19s   env=prod
+nginx-3   1/1     Running   0          19s   env=dev
+nginx-4   1/1     Running   0          19s   env=dev
+nginx-5   1/1     Running   0          19s   env=dev
