67df931e7281b5f7570e027380af19bde6f07f18
Dan Arviv <120067574+danarviv@users.noreply.github.com>
2022-12-13 02:23:03 +0200

    Create 08.sh

---
+++ Pod Design Questions/08.sh
@@ -0,0 +1,8 @@
+kubectl get pod -l env
+ 
+ NAME      READY   STATUS    RESTARTS   AGE
+nginx-1   1/1     Running   0          10m
+nginx-2   1/1     Running   0          10m
+nginx-3   1/1     Running   0          10m
+nginx-4   1/1     Running   0          10m
+nginx-5   1/1     Running   0          10m
