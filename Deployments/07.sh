2358b971f35af79372559b25d6e23c2dcc25f0ee
Dan Arviv <120067574+danarviv@users.noreply.github.com>
2022-12-14 18:27:04 +0200

    Update 07.sh

--- Deployments/07.sh
+++ Deployments/07.sh
@@ -1,1 +1,6 @@
 kubectl set image deployments/webapp nginx=repo/nginx:1.17.4 --record
+
+kubectl get deployments -o wide
+
+NAME     READY   UP-TO-DATE   AVAILABLE   AGE     CONTAINERS   IMAGES         SELECTOR
+webapp   1/1     1            1           8m57s   nginx        nginx:1.17.1   app=webapp
