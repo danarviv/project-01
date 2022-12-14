f7946e75ee167a46c01e19bcd960927e21b871f2
Dan Arviv <120067574+danarviv@users.noreply.github.com>
2022-12-14 02:22:15 +0200

    Create 01.sh

---
+++ Deployments/01.sh
@@ -0,0 +1,19 @@
+kubectl create deploy webapp --image=nginx --dry-run -o yaml > webapp.yaml
+
+kubectl apply -f webapp.yaml
+
+kubectl edit -f webapp.yaml
+
+replicas: 1 
+
+to
+
+replicas: 5
+
+kubectl get pod
+
+webapp-678fd589dc-dzwwt   1/1     Running   0          42s
+webapp-678fd589dc-sgtgs   1/1     Running   0          11s
+webapp-678fd589dc-srqc9   1/1     Running   0          11s
+webapp-678fd589dc-tngzj   1/1     Running   0          11s
+webapp-678fd589dc-vr2lp   1/1     Running   0          12s
